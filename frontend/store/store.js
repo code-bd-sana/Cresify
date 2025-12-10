1e45527ac653bbbaed21bf7ba8ab930aace07722
JoySarkarBD <developer.joysarkar@gmail.com>
2025-12-10 09:40:18 +0600

    feat: create Redux store and integrate API slices for user, product, admin, and cart features.

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -2,7 +2,6 @@ import { ProductApi } from "@/feature/ProductApi";
 import { UserApi } from "@/feature/UserApi";
 import { AdminBlogApi } from "@/feature/admin/AdminBlogApi";
 import { AdminDashboardApi } from "@/feature/admin/AdminOverviewApi";
-import { AdminProductApi } from "@/feature/admin/AdminProductApi";
 import { AdminUserApi } from "@/feature/admin/AdminUserApi";
 import { CartApi } from "@/feature/customer/CartApi";
 import { configureStore } from "@reduxjs/toolkit";
@@ -11,17 +10,19 @@ export const store = configureStore({
   reducer: {
     [UserApi.reducerPath]: UserApi.reducer,
     [ProductApi.reducerPath]: ProductApi.reducer,
+    [AdminDashboardApi.reducerPath]: AdminDashboardApi.reducer,
     [AdminBlogApi.reducerPath]: AdminBlogApi.reducer,
     [AdminUserApi.reducerPath]: AdminUserApi.reducer,
-    [CartApi.reducerPath] : CartApi.reducer
+    [CartApi.reducerPath]: CartApi.reducer,
   },
 
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat([
       UserApi.middleware,
       ProductApi.middleware,
+      AdminDashboardApi.middleware,
       AdminBlogApi.middleware,
       AdminUserApi.middleware,
-      CartApi.middleware
+      CartApi.middleware,
     ]),
 });
