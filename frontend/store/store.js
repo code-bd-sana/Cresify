09140a8154690fe5fa6ce531db11c10b09ff45e7
JoySarkarBD <developer.joysarkar@gmail.com>
2025-12-10 09:44:39 +0600

    feat: Initialize Redux store by configuring API reducers and middleware for various features.

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -2,6 +2,7 @@ import { ProductApi } from "@/feature/ProductApi";
 import { UserApi } from "@/feature/UserApi";
 import { AdminBlogApi } from "@/feature/admin/AdminBlogApi";
 import { AdminDashboardApi } from "@/feature/admin/AdminOverviewApi";
+import { AdminProductApi } from "@/feature/admin/AdminProductApi";
 import { AdminUserApi } from "@/feature/admin/AdminUserApi";
 import { CartApi } from "@/feature/customer/CartApi";
 import { configureStore } from "@reduxjs/toolkit";
@@ -9,20 +10,22 @@ import { configureStore } from "@reduxjs/toolkit";
 export const store = configureStore({
   reducer: {
     [UserApi.reducerPath]: UserApi.reducer,
+    [CartApi.reducerPath]: CartApi.reducer,
     [ProductApi.reducerPath]: ProductApi.reducer,
     [AdminDashboardApi.reducerPath]: AdminDashboardApi.reducer,
     [AdminBlogApi.reducerPath]: AdminBlogApi.reducer,
     [AdminUserApi.reducerPath]: AdminUserApi.reducer,
-    [CartApi.reducerPath]: CartApi.reducer,
+    [AdminProductApi.reducerPath]: AdminProductApi.reducer,
   },
 
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat([
       UserApi.middleware,
       ProductApi.middleware,
+      CartApi.middleware,
       AdminDashboardApi.middleware,
       AdminBlogApi.middleware,
       AdminUserApi.middleware,
-      CartApi.middleware,
+      AdminProductApi.middleware,
     ]),
 });
