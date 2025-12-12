66beb4aa478e7d9123677334fc6a8a90bce9e52d
AnkonChy <bannah76769@gmail.com>
2025-12-12 15:34:44 +0600

    change mongodb uri

--- backend/src/configure/db.js
+++ backend/src/configure/db.js
@@ -1,3 +1,33 @@
+                 
+// import dotenv from 'dotenv'
+
+// import mongoose from "mongoose"
+// dotenv.config();
+
+// const connectDB = async()=>{
+
+
+// try {
+//     const connect = await mongoose.connect('mongodb+srv://next_goal:zcQBM09IcEXIRfSc@cluster0.vsivf.mongodb.net/?appName=Cluster0', {
+//         dbName: 'cresify', 
+//       });
+//    console.log(`moongdo db connected `);
+    
+// } catch (error) {
+//     console.log(error.message, "error");
+// }
+// }
+
+// export default connectDB;
+
+
+
+
+
+
+
+
+
 
 import dotenv from 'dotenv'
 
@@ -8,8 +38,8 @@ const connectDB = async()=>{
 
 
 try {
-    const connect = await mongoose.connect('mongodb+srv://next_goal:zcQBM09IcEXIRfSc@cluster0.vsivf.mongodb.net/?appName=Cluster0', {
-        dbName: 'cresify', 
+    const connect = await mongoose.connect('mongodb://localhost:27017/', {
+        dbName: 'next-goal', 
       });
    console.log(`moongdo db connected `);
     
--- frontend/components/home/HomeBanner.jsx
+++ frontend/components/home/HomeBanner.jsx
@@ -119,6 +119,8 @@ export default function HomeBanner() {
               w-full sm:w-auto
             "
             >
+
+              {/*  */}
               Browse Services
             </button>
            </Link>
